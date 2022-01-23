2facfae5bd3c919553a912ed8fd6773ce0de358d
anshul <anshuljangwal006@gmail.com>
2022-01-24 04:28:27 +0530

    up

--- CompleteJavascript/$6Strings.js
+++ CompleteJavascript/$6Strings.js
@@ -5,8 +5,31 @@
 // console.log(doubleQuotesString) ;
 
 let str = "Anshul Jangwal is not happy with his life" ;
-let ch = str.charAt(4) ;
-let substr = str.substring(2, 7) ;
+// let ch = str.charAt(4) ;
+// let substr = str.substring(2, 7) ;
 
-console.log(ch) ;
-console.log(substr) ;
+// console.log(ch) ;
+// console.log(substr) ;
+
+// let ascii = str.charCodeAt(3) ;
+// console.log(ascii) ;
+
+//split and join function in string..
+
+//split
+let arr = str.split(" ") ;
+let arr2 = str.split("") ;
+// console.log(arr) ;
+// console.log(arr2) ;
+
+//join function..
+arr.join("$") ;
+console.log(arr) ;
+
+//trim function
+let string2 = "    How are you        " ;
+string2 = string2.trim() ;
+let st = arr.join("+") ;
+
+console.log(string2) ;
+console.log(st) ;
