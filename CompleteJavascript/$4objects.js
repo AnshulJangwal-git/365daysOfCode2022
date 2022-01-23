1b9039872f058c9ce6218779825172111ec317ed
anshul <anshuljangwal006@gmail.com>
2022-01-24 03:43:50 +0530

    up

--- CompleteJavascript/$4objects.js
+++ CompleteJavascript/$4objects.js
@@ -6,6 +6,7 @@
 let player = {
     name : "Virat kohli" ,
     age : 33 ,
+    isBowler : true,
     skillset : "Batsman" ,
 
     address : {
@@ -24,12 +25,38 @@ let player = {
 } ;
 
 //Get
-console.log(player.name) ;
-console.log(player.age) ;
-console.log(player.records[1]) ;
-player.sayHi() ;
-let returnedVal = player.totalTestMatchesPlayed() ;
-console.log(returnedVal) ;
+// console.log(player.name) ;
+// console.log(player.age) ;
+// console.log(player.records[1]) ;
+// player.sayHi() ;
+// let returnedVal = player.totalTestMatchesPlayed() ;
+// console.log(returnedVal) ;
+
+// console.log("-------------------------------------------------") ;
+
+// console.log("player :", player) ;
+// //SetorUpdate 
+// player.age = 34 ;
+// player.friends = ["Shikhar Dhawan", "Ajinkya Rahane", "Chahal", "Kuldeep"] ;
+// player.isBowler = false ;
+
+// console.log("player :", player) ;
+
+// console.log("-------------------------------") ;
+
+//delete
+// delete player.address ;
+// console.log(player + " is player") ;
+
+// player.friends = ["shikhar dhawan", "Ajinkya", "chahal"] ;
+
+// for(let key in player){
+//     console.log(key , " : ", player[key]) ;
+// }
 
 
+//get from the object using square bracket..
+let key = "age" ;
+console.log(player[key]) ;
+console.log(player.age) ; //Note both are the same things just player[key] takes a value of key and search in the object the value of of that key in the object..
 
