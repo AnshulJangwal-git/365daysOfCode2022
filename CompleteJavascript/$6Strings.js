656240f1fa73c127d3d8416f98a8b4e5761ae025
anshul <anshuljangwal006@gmail.com>
2022-01-24 04:16:20 +0530

    up

---
+++ CompleteJavascript/$6Strings.js
@@ -0,0 +1,12 @@
+// let singleQuotesString = 'single quotes String' ;
+// let doubleQuotesString = "double quotes String" ;
+
+// console.log(singleQuotesString) ;
+// console.log(doubleQuotesString) ;
+
+let str = "Anshul Jangwal is not happy with his life" ;
+let ch = str.charAt(4) ;
+let substr = str.substring(2, 7) ;
+
+console.log(ch) ;
+console.log(substr) ;
