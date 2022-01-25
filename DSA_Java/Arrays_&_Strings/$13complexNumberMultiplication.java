1d0185847a6901c2f095213a2ad015c0bb113d6b
anshul <anshuljangwal006@gmail.com>
2022-01-25 21:13:44 +0530

    up

---
+++ DSA_Java/Arrays_&_Strings/$13complexNumberMultiplication.java
@@ -0,0 +1,20 @@
+import java.util.* ;
+class $13complexNumberMultiplication {
+    
+    public static String complexNumberMultiply(String num1, String num2) {
+        int a = Integer.parseInt(num1.substring(0, num1.indexOf('+'))) ;
+        int b = Integer.parseInt(num1.substring(num1.indexOf('+') + 1, num1.length() - 1)) ;
+    
+        int c = Integer.parseInt(num2.substring(0, num2.indexOf('+'))) ;
+        int d = Integer.parseInt(num2.substring(num2.indexOf('+') + 1, num2.length() - 1)) ;
+    
+        int realPart = (a * c - b * d) ;
+        int imgPart = (a * d + c * b) ;
+    
+    
+        return "" + realPart + "+" + imgPart  + "i" ;
+        
+      }
+        
+        
+    }
