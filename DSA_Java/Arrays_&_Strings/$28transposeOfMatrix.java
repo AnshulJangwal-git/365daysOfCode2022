a8bd3cc2929456393e9d71d62829026562741e67
anshul <anshuljangwal006@gmail.com>
2022-01-30 21:51:06 +0530

    up

---
+++ DSA_Java/Arrays_&_Strings/$28transposeOfMatrix.java
@@ -0,0 +1,14 @@
+class $28transposeOfMatrix {
+    public int[][] transpose(int[][] matrix) {
+        int[][] res = new int[matrix[0].length][matrix.length] ;
+        
+        for(int i = 0; i < res.length; i++){
+            for(int j = 0; j < res[0].length; j++){
+                res[i][j] = matrix[j][i] ;
+            }
+        }
+        
+        return res ;
+    }
+    
+}
