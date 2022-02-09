bff2322131b386ae9963c6266ab82781495b7b56
anshuljangwal-git <anshuljangwal@gmail.com>
2022-02-09 11:55:41 +0530

    up

--- AApractice/Main.java
+++ AApractice/Main.java
@@ -7,8 +7,7 @@ import java.io.* ;
 public class Main {
     public static void main(String[] args){
         // Scanner sc = new Scanner(System.in) ;
-        Integer arr[] = {3, 4, 3, 13, 21, 4, 27, 56, 54, 54, 13, 22, 89, 98, 101, -4, -12, -1, 0, -1, -2, 0, -4, -13, -11, -12, -12, 9, 0, -7} ;
-        int n = arr.length ;
+        System.out.println("Hello world") ;
 
         //lambda function...(Sorting on the basis of absolute values)
         // Arrays.sort(arr, (a, b) -> {
@@ -39,27 +38,27 @@ public class Main {
         // }
         // System.out.println(set) ;
         
-        Arrays.sort(arr) ;
+        // Arrays.sort(arr) ;
         
-        int min1 = arr[0] ;
-        int min2 = arr[1] ;
+        // int min1 = arr[0] ;
+        // int min2 = arr[1] ;
 
-        int max1 = arr[n - 1] ;
-        int max2 = arr[n - 2] ;
-        int max3 = arr[n - 3] ;
+        // int max1 = arr[n - 1] ;
+        // int max2 = arr[n - 2] ;
+        // int max3 = arr[n - 3] ;
         
-        for(int val : arr){
-            System.out.print(val + " ") ;
-        }
-        System.out.println() ;
+        // for(int val : arr){
+        //     System.out.print(val + " ") ;
+        // }
+        // System.out.println() ;
         
-        System.out.println(min1) ;
-        System.out.println(min2) ;
-        System.out.println(max1) ;
-        System.out.println(max2) ;
-        System.out.println(max3) ;
+        // System.out.println(min1) ;
+        // System.out.println(min2) ;
+        // System.out.println(max1) ;
+        // System.out.println(max2) ;
+        // System.out.println(max3) ;
 
-
+        
     }
 
 
