a4ebe082f1948de9669eefdc05f7feb0c5c7d21e
anshuljangwal-git <anshuljangwal@gmail.com>
2022-02-11 22:09:54 +0530

    up

--- DSA_Java/Arrays_&_Strings/$51meetingRooms2.java
+++ DSA_Java/Arrays_&_Strings/$51meetingRooms2.java
@@ -42,3 +42,55 @@ public class $51meetingRooms2 {
   }
 }
 
+//Method2(Cronological Order)
+
+// import java.util.*;
+
+// public class Main {
+//   public static int meetingRooms(int intervals[][]) {
+//     int[] sp = new int[intervals.length] ;
+//     int[] ep = new int[intervals.length] ;
+    
+//     for(int i = 0; i < intervals.length; i++){
+//         sp[i] = intervals[i][0] ;
+//         ep[i] = intervals[i][1] ;
+//     }
+    
+//     Arrays.sort(sp) ;
+//     Arrays.sort(ep) ;
+    
+//     int i = 0 ;
+//     int j = 0 ;
+//     int roomCount = 0 ;
+    
+//     while(i < sp.length){
+//         if(sp[i] < ep[j]){
+//             roomCount ++ ;
+//             i ++ ;
+            
+//         }else{
+//             i ++ ;
+//             j ++ ;
+//         }
+//     }
+//     return roomCount ;
+//   }
+//   public static void main(String args[]) {
+//     Scanner scn = new Scanner(System.in);
+
+//     // Input Format
+//     int n = scn.nextInt();
+//     int input[][] = new int[n][2];
+
+//     for (int i = 0 ; i <  n ; i++) {
+//       int sp = scn.nextInt();
+//       int ep = scn.nextInt();
+
+//       input[i][0] = sp;
+//       input[i][1] = ep;
+//     }
+
+//     // Output Format
+//     System.out.println(meetingRooms(input));
+//   }
+// }
