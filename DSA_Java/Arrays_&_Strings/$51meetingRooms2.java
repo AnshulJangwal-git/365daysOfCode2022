894869a38e3f1709dfa92d012d9c0394205542e1
anshuljangwal-git <anshuljangwal@gmail.com>
2022-02-11 21:38:31 +0530

    up

---
+++ DSA_Java/Arrays_&_Strings/$51meetingRooms2.java
@@ -0,0 +1,44 @@
+//Link - https://pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/arrays-and-strings/meeting_room_two_official/ojquestion
+import java.util.*;
+
+public class $51meetingRooms2 {
+  public static int meetingRooms(int intervals[][]) {
+    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0])) ;
+    PriorityQueue<Integer> pq = new PriorityQueue<>() ;
+    
+    for(int[] interval : intervals){
+        if(pq.size() == 0){
+            pq.add(interval[1]) ;
+            
+        }else{
+            if(pq.peek() > interval[0]){
+                pq.add(interval[1]) ;
+                
+            }else{
+                pq.remove() ;
+                pq.add(interval[1]) ;
+            }
+        }
+    }
+    return pq.size() ;
+  }
+  public static void main(String args[]) {
+    Scanner scn = new Scanner(System.in);
+
+    // Input Format
+    int n = scn.nextInt();
+    int input[][] = new int[n][2];
+
+    for (int i = 0 ; i <  n ; i++) {
+      int sp = scn.nextInt();
+      int ep = scn.nextInt();
+
+      input[i][0] = sp;
+      input[i][1] = ep;
+    }
+
+    // Output Format
+    System.out.println(meetingRooms(input));
+  }
+}
+
