8b50bb238093967ea1f6a26fa36dbefd806c0f96
anshuljangwal-git <anshuljangwal@gmail.com>
2022-02-20 22:00:41 +0530

    up

--- DSA_Java/SearchingAndSorting/$10maximisetheSum.java
+++ DSA_Java/SearchingAndSorting/$10maximisetheSum.java
@@ -1,28 +1,16 @@
-//Link - https://nados.io/question/maximize-sum-of-arr-i-i-of-an-array
-import java.util.*;
-
-public class $10maximisetheSum {
-
-    public static int maximise(int[]arr) {
+//Link - https://practice.geeksforgeeks.org/problems/maximize-arrii-of-an-array0026/1/#
+import java.util.* ;
+class $10maximisetheSum{
+    
+    int Maximize(int arr[], int n)
+    {
+        // Complete the function
         Arrays.sort(arr) ;
-        int sum = 0 ;
+        long sum = 0 ;
         for(int i = 0; i < arr.length; i++){
-            sum += arr[i]*i ;
+            sum += (long)arr[i] * i ;
         }
-        return sum ;
-    }
-
-    public static void main(String[]args) {
-        //input work
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-
-        int[]arr = new int[n];
-        for(int i = 0 ; i < n; i++) {
-            arr[i] = scn.nextInt();
-        }
-
-        int ans = maximise(arr);
-        System.out.println(ans);
-    }
+        int mod = (int)(Math.pow(10, 9) + 7) ;
+        return (int)(sum % mod) ;
+    }   
 }
