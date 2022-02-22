2738e5c4efd7415b40501f092846705444730f9f
anshuljangwal-git <anshuljangwal@gmail.com>
2022-02-22 12:46:28 +0530

    up

---
+++ DSA_Java/SearchingAndSorting/$16findRotationCount.java
@@ -0,0 +1,59 @@
+//Link - https://practice.geeksforgeeks.org/problems/rotation4723/1/#
+// { Driver Code Starts
+// Initial Template for Java
+
+// import java.io.*;
+// import java.util.*;
+
+// class GFG {
+//     // Driver code
+//     public static void main(String[] args) throws Exception {
+//         BufferedReader br =
+//             new BufferedReader(new InputStreamReader(System.in));
+//         int t = Integer.parseInt(br.readLine().trim());
+//         while (t-- > 0) {
+//             int n = Integer.parseInt(br.readLine().trim());
+//             String[] str = br.readLine().trim().split(" ");
+//             int[] a = new int[n];
+//             for (int i = 0; i < n; i++) {
+//                 a[i] = Integer.parseInt(str[i]);
+//             }
+
+//             int ans = new Solution().findKRotation(a, n);
+//             System.out.println(ans);
+//         }
+//     }
+// }
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class $16findRotationCount {
+    int findKRotation(int arr[], int n) {
+        int lo = 0 ;
+        int hi = arr.length - 1 ;
+        
+        if(arr[lo] <= arr[hi]){
+            return 0 ;
+        }
+        
+        while(lo <= hi){
+            int mid = (lo + hi) / 2 ;
+            if(arr[mid] > arr[mid + 1]){
+                return mid + 1 ;
+                
+            }else if(arr[mid - 1] > arr[mid]){
+                return mid ;
+                
+            }else if(arr[lo] <= arr[mid]){
+                lo = mid + 1 ;
+                
+            }else if(arr[mid] <= arr[hi]){
+                hi = mid - 1 ;
+                
+            }
+        }
+        return 0 ;
+    }
+}
