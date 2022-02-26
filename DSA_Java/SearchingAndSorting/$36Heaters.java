24d7a8ec19ee7f0d55224bd6ed449f125300c3b1
anshuljangwal-git <anshuljangwal@gmail.com>
2022-02-26 20:52:28 +0530

    up

--- DSA_Java/SearchingAndSorting/$36Heaters.java
+++ DSA_Java/SearchingAndSorting/$36Heaters.java
@@ -1,4 +1,5 @@
 //Leetcode 475 - https://leetcode.com/problems/heaters/
+import java.util.* ;
 class $36Heater {
     public int findRadius(int[] houses, int[] heaters) {
         int maxRadii = 0 ;
@@ -28,3 +29,58 @@ class $36Heater {
 
 //Method2
 //Using Binary Search
+class Solution {
+    public Pair binarySearch(int hp, int[] heaters){
+        int lo = 0 ;
+        int hi = heaters.length - 1 ;
+        Pair p = new Pair() ;
+        
+        while(lo <= hi){
+            int mid = (lo + hi) / 2 ;
+            if(heaters[mid] == hp){
+                p.js = heaters[mid] ;
+                p.jl = heaters[mid] ;
+                break ;
+            }else if(heaters[mid] < hp){
+                p.js = heaters[mid] ;
+                lo = mid + 1 ;
+                
+            }else{
+                p.jl  = heaters[mid] ;
+                hi = mid - 1 ;
+            }
+        }
+        return p ;
+    }
+    
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(heaters) ;
+        int ans = 0 ;
+        for(int i = 0; i < houses.length; i++){
+            int hp = houses[i] ;
+            Pair p = binarySearch(hp, heaters) ;
+            
+            int d1 = (p.js == -1) ? Integer.MAX_VALUE : hp - p.js ;
+            int d2 = (p.jl == -1) ? Integer.MAX_VALUE : p.jl - hp ;
+            
+            int dis = Math.min(d1, d2) ;
+            if(dis > ans){
+                ans = dis ;
+            }
+        }
+        return ans ;
+    }
+    class Pair{
+        int js = -1 ; //just smaller
+        int jl = -1 ; //just larger
+        
+        Pair(){
+            
+        }
+        
+        Pair(int js, int jl){
+            this.js = js ;
+            this.jl = jl ;
+        }
+    }
+}
