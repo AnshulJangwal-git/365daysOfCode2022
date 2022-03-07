3f7967c2040a6ed48587187470b39fd250ff0ebb
anshuljangwal-git <anshuljangwal@gmail.com>
2022-03-07 20:40:17 +0530

    up

---
+++ DSA_Java/Bit_Manipulation/$11countTripletsofEqualXOR.java
@@ -0,0 +1,16 @@
+//Leetcode 1442 - https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor/
+class $11countTripletsofEqualXOR {
+    public int countTriplets(int[] arr) {
+        int count = 0 ;
+        for(int i = 0; i < arr.length; i++){
+            int val = arr[i] ;
+            for(int k = i + 1; k < arr.length; k++){
+                val = val ^ arr[k] ;
+                if(val == 0){
+                    count += (k - i) ;
+                }
+            }
+        }
+        return count ;
+    }
+}
