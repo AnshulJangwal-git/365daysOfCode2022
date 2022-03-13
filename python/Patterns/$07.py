030540aa61deb0c702821ea403df10979ca8f8e8
anshuljangwal-git <anshuljangwal@gmail.com>
2022-03-13 20:58:23 +0530

    up

---
+++ python/Patterns/$07.py
@@ -0,0 +1,11 @@
+n = int(input())
+sp = 0 
+
+for i in range(1, n + 1) :
+    for j in range(1, sp + 1) :
+        print(end = "\t")
+    
+    print(end = "*")
+    
+    sp += 1
+    print(end = "\n")
