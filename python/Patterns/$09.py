146e202103f9acc551ba08ab999e1bf231c6c298
anshuljangwal-git <anshuljangwal@gmail.com>
2022-03-13 21:41:27 +0530

    up

---
+++ python/Patterns/$09.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(n) :
+    for j in range(n) :
+        
+        if( (i == j) or (i + j == n - 1)) :
+            print("*\t", end = "")
+        
+        else :
+            print("\t", end = "")
+    print()
