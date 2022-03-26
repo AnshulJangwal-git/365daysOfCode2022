2eb338e3d68e0bd00d4e9c3fca0b212f1b07e026
anshuljangwal-git <anshuljangwal@gmail.com>
2022-03-26 13:09:20 +0530

    up

--- DSA_Java/StacksAndQueues/$26TrappingRainWater.java
+++ DSA_Java/StacksAndQueues/$26TrappingRainWater.java
@@ -22,3 +22,29 @@ class $26TrappingRainWater {
     } 
     
 }
+
+// Using O(1) Space and Time
+class Solution {
+    public int trap(int[] h) {
+        int l = 0 ;
+        int r = h.length - 1 ;
+        
+        int ans = 0 ;
+        int lmax = 0, rmax = 0 ;
+        
+        while(l < r){
+            lmax = Math.max(lmax, h[l]) ;
+            rmax = Math.max(rmax, h[r]) ;
+            
+            if(lmax < rmax){
+                ans += lmax - h[l] ;
+                l ++ ;
+                
+            }else{
+                ans += rmax - h[r] ;
+                r -- ;
+            }
+        }
+        return ans ;
+    }
+}
