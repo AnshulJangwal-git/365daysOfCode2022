9f3307d815e15b52a6d4e8681707e6ca0334c474
anshuljangwal-git <anshuljangwal@gmail.com>
2022-03-29 02:26:43 +0530

    up

---
+++ DSA_Java/Arrays_&_Strings/$66buildArrayFromPermutation.java
@@ -0,0 +1,12 @@
+// Leetcode 1920 - https://leetcode.com/problems/build-array-from-permutation/
+class $66buildArrayFromPermutation {
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length] ;
+        
+        for(int i = 0; i < nums.length; i++){
+            ans[i] = nums[nums[i]] ;
+        }
+        
+        return ans ;
+    }
+}
