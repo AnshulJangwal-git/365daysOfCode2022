823c027cd9271929767524b3a79426fe4c5ea390
anshuljangwal-git <anshuljangwal@gmail.com>
2022-03-30 22:03:36 +0530

    up

--- DSA_Java/sortingAlgos/$02selectionSort.java
+++ DSA_Java/sortingAlgos/$02selectionSort.java
@@ -1,23 +1,13 @@
-import java.util.* ;
-//time Complexity 
-            // => worst Case - O(n^2)
-            // Best case = O(n)
+import java.io.*;
+import java.util.*;
+// Time Complexity -> Worst Case - O(n^2)
+                    //   Best Case - O(n)
+
 
 public class $02selectionSort {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in) ;
-        int n = sc.nextInt() ;
-        int[] arr = new int[n] ;
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = sc.nextInt() ;
-        }
 
-        selectionSort(arr) ;
-        print(arr) ;
-    }
-
-    public static void selectionSort(int[] arr){
-        for(int i = 0; i < arr.length; i++){
+  public static void selectionSort(int[] arr){
+        for(int i = 0; i < arr.length - 1; i++){
             int min = i ;
             for(int j = i + 1; j < arr.length; j++){
                 if(isSmaller(arr, j, min) == true){
@@ -28,26 +18,39 @@ public class $02selectionSort {
         }
     }
 
-    public static boolean isSmaller(int[] arr, int i, int j){
-        if(arr[i] < arr[j]){
-            return true ;
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
 
-        }else{
-            return false ;
-
-        }
+  // return true if ith element is smaller than jth element
+  public static boolean isSmaller(int[] arr, int i, int j) {
+    System.out.println("Comparing " + arr[i] + " and " + arr[j]);
+    if (arr[i] < arr[j]) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    public static void swap(int[] arr, int i, int j){
-        int temp = arr[i] ;
-        arr[i] = arr[j] ;
-        arr[j] = temp ;
+  public static void print(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
     }
+  }
 
-    public static void print(int[] arr){
-        for(int val : arr){
-            System.out.print(val + " ") ;
-        }
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
     }
+    selectionSort(arr);
+    print(arr);
+  }
 
 }
