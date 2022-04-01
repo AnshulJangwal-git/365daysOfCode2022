5927fc5ac147719b2551163c3fe4da5e167a88a8
anshuljangwal-git <anshuljangwal@gmail.com>
2022-04-01 13:55:22 +0530

    up

---
+++ DSA_Java/Vector/Intro/vector1.java
@@ -0,0 +1,15 @@
+import java.util.* ;
+
+public class vector1 {
+    public static void main(String[] args){
+        Vector v = new Vector() ;
+        v.add(1) ;
+        v.add("VIT Vellore") ;
+        v.add("Anshul Jangwal") ;
+        v.add(3) ;
+        v.add('l') ;
+        v.add(3.45) ;
+
+        System.out.println("Vector is -> " + v) ;
+    }    
+}
