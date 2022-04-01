4be5e0b504bbda8b4532c1336e240d93ab5a4f90
anshuljangwal-git <anshuljangwal@gmail.com>
2022-04-01 14:23:38 +0530

    up

--- DSA_Java/Vector/Intro/vector2.java
+++ DSA_Java/Vector/Intro/vector2.java
@@ -9,12 +9,20 @@ public class vector2 {
         v.add(1, 1) ;
         v.add(2, "B.tech") ;
         v.add(3, 2) ;
-        System.out.println("Vector is -> " + v) ;
+        // System.out.println("Vector is -> " + v) ;
         // Vector is -> [Anshul Jangwal, 1, B.tech, 2] // size = 4
 
         v.add(1, "VIT") ;
 
-        System.out.println("Vector is -> " + v) ;
+        // System.out.println("Vector is -> " + v) ;
         // Vector is -> [Anshul Jangwal, VIT, 1, B.tech, 2]  //size = 5
+
+        // System.out.println("size is " + v.size()) ;
+
+        // System.out.println(v.firstElement()) ;
+
+        System.out.println(v.get(1)) ;
+
+
     }
 }
