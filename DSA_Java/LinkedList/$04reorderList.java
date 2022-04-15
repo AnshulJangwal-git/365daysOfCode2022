38b57e007fdfedc85adad10c0330549c49674b51
anshuljangwal-git <anshuljangwal@gmail.com>
2022-04-15 10:16:12 +0530

    up

---
+++ DSA_Java/LinkedList/$04reorderList.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class $04reorderList {
+    
+    public ListNode middle(ListNode head){
+        ListNode slow = head ;
+        ListNode fast = head ;
+        
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next ;
+            fast = fast.next.next ;
+        }
+        
+        return slow ;
+    }
+    
+    public ListNode reverse(ListNode head){
+        ListNode prev = null ;
+        ListNode curr = head ;
+        
+        while(curr != null){
+            ListNode next = curr.next ;
+            curr.next = prev ;
+            prev = curr ;
+            curr = next ;
+        }
+        return prev ;
+    }
+    
+    public void reorderList(ListNode head) {
+        ListNode mid = middle(head) ;
+        ListNode nh = mid.next ;
+        
+        mid.next = null ;
+        ListNode left = head ;
+        ListNode right = reverse(nh) ;
+        
+        while(right != null){
+            ListNode leftNext = left.next ;
+            ListNode rightNext = right.next ;
+
+            left.next = right ;
+            right.next = leftNext ;
+
+            left = leftNext ;
+            right = rightNext ;  
+        }
+    }
+}
