3d60c2bcda1decd109993eae73d62fdf9fc6da40
anshuljangwal-git <anshuljangwal@gmail.com>
2022-04-17 09:52:13 +0530

    up

---
+++ DSA_Java/LinkedList/$09removeNthfromList.java
@@ -0,0 +1,29 @@
+// Leetcode 19 - https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class $09removeNthfromList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode slow = head ;
+        ListNode fast = head ;
+        
+        for(int i = 0; i < n; i++){
+            fast = fast.next ;
+        }
+        if(fast == null) return head.next ;
+        
+        while(fast.next != null){
+            fast = fast.next ;
+            slow = slow.next ;
+        }
+        slow.next = slow.next.next ;
+        return head ;
+    }
+}
