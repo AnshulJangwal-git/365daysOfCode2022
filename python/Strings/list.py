741f0f971ae838b78e943fc730cb4bb112d2ec28
anshuljangwal-git <anshuljangwal@gmail.com>
2022-04-22 12:09:25 +0530

    up

---
+++ python/Strings/list.py
@@ -0,0 +1,37 @@
+names = ["rohan", "nitin", "soniya", "ananya", "sachin"]
+# print(names)
+# for name in names :
+#     print(name)
+
+
+num = [24, 3, 14, 16, 5, 11]
+# sort() method - sort an array in non-decreasing order
+# num.sort()
+# print(num)
+
+# min and max
+minVal = min(num)
+# print(minVal)
+
+maxVal = max(num)
+# print(maxVal)
+
+num.append(56)
+# print(num)
+# print(len(num))
+
+num.insert(2, 5)
+# print(num)
+
+# remove() function
+num.remove(11)
+# print(num)
+
+num.pop()
+# remove last element of list
+# print(num)
+
+# list = [11, 36, 55, 42, 21, 103, 23, 12]
+# for i in range(0, len(list)) :
+#     if(i % 2 == 0) :
+#         print(list[i])
