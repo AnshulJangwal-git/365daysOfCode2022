6c68beacbd77bc3c7c68faafc0f42d11ace4f50f
anshuljangwal-git <anshuljangwal@gmail.com>
2022-04-30 00:57:14 +0530

    up

---
+++ DSA_Java/LinkedList/$27mergeNodesInBetweenZeros.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class $27mergeNodesInBetweenZeros {
+    public ListNode mergeNodes(ListNode head) {
+        if(head == null){
+            return head ;
+        }
+        
+        int sum = 0 ;
+        
+        ListNode dummy = new ListNode(-1) ;
+        ListNode dn = dummy ;
+        ListNode curr = head.next ;
+        
+        while(curr != null){
+            while(curr.val != 0){
+                sum += curr.val ;
+                curr = curr.next ;
+            }
+            dn.next = new ListNode(sum) ;
+            dn = dn.next ;
+            curr = curr.next ;
+            sum = 0 ;
+        }
+        return dummy.next ;
+    }
+}
