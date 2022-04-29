e62e64f6d99bcecacfc6ee8f079e51df9c8a55bd
anshuljangwal-git <anshuljangwal@gmail.com>
2022-04-30 02:55:02 +0530

    up

---
+++ DSA_Java/LinkedList/$34deleteNodeInLinkedList.java
@@ -0,0 +1,15 @@
+// Leetcode 237 - https://leetcode.com/problems/delete-node-in-a-linked-list/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class $34deleteNodeInLinkedList {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val ;
+        node.next = node.next.next ;
+    }
+}
