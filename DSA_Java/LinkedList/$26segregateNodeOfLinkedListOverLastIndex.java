2afced856785197773663a624fa125ebae26e326
anshuljangwal-git <anshuljangwal@gmail.com>
2022-04-29 23:19:24 +0530

    up

---
+++ DSA_Java/LinkedList/$26segregateNodeOfLinkedListOverLastIndex.java
@@ -0,0 +1,91 @@
+import java.util.*;
+
+class $26segregateNodeOfLinkedListOverLastIndex {
+  public static Scanner scn = new Scanner(System.in);
+
+  public static class ListNode {
+    int val = 0;
+    ListNode next = null;
+
+    ListNode(int val) {
+      this.val = val;
+    }
+  }
+
+  public static ListNode segregate(ListNode head, int pivotIdx) {
+    if(head == null || head.next == null){
+        return head ;
+    }
+    
+    int idx = 0 ;
+    int data = 0 ;
+    
+    ListNode curr = head ;
+    
+    while(curr != null){
+        if(idx == pivotIdx){
+            data = curr.val ;
+            break ;
+        }    
+        curr = curr.next ;
+        idx += 1 ;
+    }
+    curr = head ;
+    ListNode smaller = new ListNode(-1) ;
+    ListNode ps = smaller ;
+    ListNode larger = new ListNode(-1) ;
+    ListNode pl = larger ;
+    
+    ListNode pivot = null ;
+    idx = 0 ;
+    
+    while(curr != null){
+        if(idx == pivotIdx){
+            pivot = curr ;
+            // pivot.val = curr.val ;
+            
+        }else if(curr.val <= data){
+            ps.next = curr ;
+            ps = ps.next ;
+            
+        }else{
+            pl.next = curr ;
+            pl = pl.next ;
+            
+        }
+        curr = curr.next ;
+        idx += 1 ;
+    }
+    ps.next = pivot ;
+    pivot.next = larger.next ;
+    pl.next = null ;
+    
+    return smaller.next ;
+  }
+
+  public static void printList(ListNode node) {
+    while (node != null) {
+      System.out.print(node.val + " ");
+      node = node.next;
+    }
+  }
+
+  public static ListNode createList(int n) {
+    ListNode dummy = new ListNode(-1);
+    ListNode prev = dummy;
+    while (n-- > 0) {
+      prev.next = new ListNode(scn.nextInt());
+      prev = prev.next;
+    }
+
+    return dummy.next;
+  }
+
+  public static void main(String[] args) {
+    int n = scn.nextInt();
+    ListNode h1 = createList(n);
+    int idx = scn.nextInt();
+    h1 = segregate(h1, idx);
+    printList(h1);
+  }
+}
