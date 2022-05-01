5b01bcec3ac188e6af28a6f6ad60eca95bbb3f3f
anshuljangwal-git <anshuljangwal@gmail.com>
2022-05-01 17:01:37 +0530

    up

--- DSA_Java/Arrays_&_Strings/$01longPressedName.java
+++ DSA_Java/Arrays_&_Strings/$01longPressedName.java
@@ -1,49 +1,30 @@
-
-import java.util.*;
-
-public class $01longPressedName {
-
-    // ~~~~~~~~~~~~~~~~~~~~~User Section~~~~~~~~~~~~~~~~~~~~~
-
-    public static boolean isPossible(String name, String typed) {
-        if(name.length() > typed.length()){
-            return false ;
-        }
-        
-        int i = 0 ; 
+// Leetcode 925 - https://leetcode.com/problems/long-pressed-name/
+class $01longPressedName {
+    public boolean isLongPressedName(String name, String typed) {
+        int i = 0 ;
         int j = 0 ;
         
-        while(j < typed.length()){
-            if(i < name.length() && name.charAt(i) == typed.charAt(j)){
+        int n = name.length() ;
+        int t = typed.length() ;
+        
+        while(i < n && j < t){
+            if(name.charAt(i) == typed.charAt(j)){
                 i ++ ;
                 j ++ ;
                 
-            }else if(j > 0 && typed.charAt(j) == typed.charAt(j - 1)){
+            }else if(i > 0 && name.charAt(i - 1) == typed.charAt(j)){
                 j ++ ;
                 
             }else{
                 return false ;
-                
             }
         }
         
-        if(i != name.length()){
-            return false ;
-        }    
-        
-        return true ;
-    }
-
-    // ~~~~~~~~~~~~~~~~~~~Input Management~~~~~~~~~~~~~~~~~~~
-
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        
-        String name = scn.next();
-        String typed = scn.next();
-
-        boolean res = isPossible(name, typed);
-
-        System.out.println(res);
+        while(j < t){
+            if(typed.charAt(j) != name.charAt(i - 1)) return false ;
+            j ++ ;
+        }
+        if(i < n)return false ;
+        else return true ;
     }
 }
