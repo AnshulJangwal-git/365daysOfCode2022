aacb6dfc8948f39a06d942661e0c7f4a8c2010ce
anshuljangwal-git <anshuljangwal@gmail.com>
2022-07-08 23:21:04 +0530

    up

--- DSA_Java/Recursion&Backtracking/Recursion with Arrays/$05lastIndex.java
+++ DSA_Java/Recursion&Backtracking/Recursion with Arrays/$05lastIndex.java
@@ -30,3 +30,46 @@ public class $05lastIndex {
     }
 
 }
+
+
+
+
+// import java.io.*;
+// import java.util.*;
+// Method2
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in) ;
+        int n = sc.nextInt() ;
+        int arr[] = new int[n] ;
+        
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt() ;
+        }
+        int x = sc.nextInt() ;
+        
+        int li = lastIndex(arr, 0, x) ;
+        System.out.println(li) ;
+    }
+
+    public static int lastIndex(int[] arr, int idx, int x){
+        if(idx == arr.length) return -1 ;
+        
+        int lisa = lastIndex(arr, idx + 1, x) ;
+        if(lisa == -1){
+            if(arr[idx] == x){
+                return idx ;
+                
+            }else{
+                return -1 ;
+                
+            }
+        }else{
+            return lisa ;
+            
+        }
+    }
+
+}
